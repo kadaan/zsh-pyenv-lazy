be187f029afb1c5dfbc854355423269b1d11c10a
Joel Baranick <joel.baranick@ensighten.com>
2018-02-04 23:17:47 -0800

    Fix formatting

--- pyenv-lazy.plugin.zsh
+++ pyenv-lazy.plugin.zsh
@@ -1,10 +1,10 @@
 # Lazy load pyenv
 if type pyenv &> /dev/null; then
-    local PYENV_SHIMS="${PYENV_ROOT:-${HOME}/.pyenv}/shims"
-    export PATH="${PYENV_SHIMS}:${PATH}"
-    function pyenv() {
-        unset pyenv
-        eval "$(command pyenv init -)"
-        pyenv $@
-    }
+  local PYENV_SHIMS="${PYENV_ROOT:-${HOME}/.pyenv}/shims"
+  export PATH="${PYENV_SHIMS}:${PATH}"
+  function pyenv() {
+    unset pyenv
+    eval "$(command pyenv init -)"
+    pyenv $@
+  }
 fi
