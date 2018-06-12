86967679a6d86e206f9138d7aa55b81514613e1c
Joel Baranick <joel.baranick@ensighten.com>
2018-06-12 14:35:44 -0700

    Fix the unset command

--- pyenv-lazy.plugin.zsh
+++ pyenv-lazy.plugin.zsh
@@ -3,7 +3,7 @@ if type pyenv &> /dev/null; then
   local PYENV_SHIMS="${PYENV_ROOT:-${HOME}/.pyenv}/shims"
   export PATH="${PYENV_SHIMS}:${PATH}"
   function pyenv() {
-    unset pyenv
+    unset -f pyenv > /dev/null 2>&1
     eval "$(command pyenv init -)"
     pyenv $@
   }
